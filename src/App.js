9939b249a8b8751566992767c6c2dca4fd90edbf
meng <wulalameng@gmail.com>
2017-09-02 11:22:13 -0400

    create handleRemoveToCart function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 import './App.css';
 import ItemPage from './ItemPage';
+import CartPage from './CartPage';
 import {items} from './static-data';
 
 class App extends Component {
@@ -23,23 +24,59 @@ class App extends Component {
     });
   }
 
-  renderContent() {
+  handleRemoveToCart = (item) => {
+    let newCart = this.state.cart.slice(0);
+    let idx = newCart.indexOf(item.id);
+    if(idx !== -1) {
+      newCart.splice(idx, 1);
+      this.setState({
+        cart: newCart
+      });
+    }
+  }
+
+  renderContent = () => {
     let content;
     switch (this.state.activeTab) {
       case 0:
         content = <ItemPage
           items={items}
-          onAddToCart={this.handleAddToCart}
-                  />;
+          onAddToCart={this.handleAddToCart} />;
         break;
       case 1:
-        content = <span>Cart</span>;
+        content = this.renderCartPage();
         break;
       default:
         content = <span>Empty</span>;
     }
     return content;
-  }
+  };
+
+  renderCartPage= () =>{
+    // Count how many of each items is in the cart
+    let itemCounts = this.state.cart.reduce((itemCounts, itemId) => {
+      itemCounts[itemId] = itemCounts[itemId] || 0;
+      itemCounts[itemId] ++;
+      return itemCounts;
+    }, {});
+
+    // Create an array of items
+    let cartItems = Object.keys(itemCounts).map(itemId => {
+      // Find the item by its id
+      let item = items.find( item => item.id === parseInt(itemId, 10));
+      return {
+        ...item,
+        count: itemCounts[itemId]
+      };
+    });
+
+    return (
+      <CartPage items={cartItems}
+        onAddOne={this.handleAddToCart}
+        onRemoveOne={this.handleRemoveToCart}
+      />
+    );
+  };
 
   render() {
     const {activeTab} = this.state;
