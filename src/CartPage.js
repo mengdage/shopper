24ff432d712d7d467c63c3513f835d37e3777cf9
meng <wulalameng@gmail.com>
2017-09-02 11:35:17 -0400

    add empty message

--- src/CartPage.js
+++ src/CartPage.js
@@ -4,27 +4,38 @@ import Item from './Item';
 import './CartPage.css';
 
 function CartPage({items, onAddOne, onRemoveOne}) {
+  const len = items.length;
   return (
-    <ul className='cartpage-items'>
-      {items.map( item => (
-        <li key={item.id}
-        className='cartpage-item'>
-          <Item item={item}>
-            <div className='carpage-control'>
-              <button
-                className='cartpage-removeone'
-                onClick={()=>onRemoveOne(item)}
-              >-</button>
-              <span className='cartpage-count'>{item.count}</span>
-              <button
-                className='cartpage-addone'
-                onClick={()=>onAddOne(item)}
-              >+</button>
+    (len> 0) ? (
+      <ul className='cartpage-items'>
+        {items.map( item => (
+          <li key={item.id}
+          className='cartpage-item'>
+            <Item item={item}>
+              <div className='carpage-control'>
+                <button
+                  className='cartpage-removeone'
+                  onClick={()=>onRemoveOne(item)}
+                >-</button>
+                <span className='cartpage-count'>{item.count}</span>
+                <button
+                  className='cartpage-addone'
+                  onClick={()=>onAddOne(item)}
+                >+</button>
+              </div>
+            </Item>
+            <div>
+              Total: $2032
             </div>
-          </Item>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <div className='cartpage-empty'>
+        <p>You cart is empty.</p>
+        <p>Why not adding some products?</p>
+      </div>
+    )
   );
 }
 
